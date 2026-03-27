2263735d3b30effda3dccbf785f8cf4315e7ff9c
Konstantine Tsirgvava <koka.wirgvava@gmail.com>
2026-03-27 19:00:54 +0400

    initial commit

---
+++ ShipsTests/ShipsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShipsTests.swift
+//  ShipsTests
+//
+//  Created by Konstantine Tsirgvava on 27/03/2026.
+//
+
+import Testing
+@testable import Ships
+
+struct ShipsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
